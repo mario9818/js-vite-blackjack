568b19a2be99c2870c861c282485ca76beec09df
Mario Suarez <mariosuareztabares@gmail.com>
2026-04-26 23:13:19 -0500

    first commit

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,49 @@
+import { crearCartaHTML } from './crear-carta-HTML';
+import { pedirCarta } from './pedir-carta';
+import { valorCarta } from './valor-carta';
+
+
+/**
+ * 
+ * @param {Number} puntosMinimos puntos mínimos que la PC necesita para ganar
+ * @param {HTMLElement} puntosHTML elemento HTML para mostrar los puntos
+ * @param {HTMLElement} divCartasComputadora elemento HTML para mostrar las cartas
+ * @param {Array<String>} deck 
+ */
+
+// turno de la computadora
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if (!puntosMinimos) throw new Error ('Puntos mínimos son necesarios');
+    if (!puntosHTML) throw new Error ('Argumento puntosHTML es necesario');
+
+    let puntosComputadora = 0;
+    
+
+    do {
+        const carta = pedirCarta(deck);
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+       const imgCarta = crearCartaHTML (carta);
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
